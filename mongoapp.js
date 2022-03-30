6c89a513afea56103a3925f1888ac988693c16d2
p00149021@houstonisd.org Mag17615@7 <lioneljones5116@gmail.com>
2022-03-30 13:19:24 -0500

    Addd YAML file

---
+++ mongoapp.js
@@ -0,0 +1,24 @@
+//this code was taken from the official docs
+//https://mongodb.github.io/node-mongodb-native/3.5/quick-start/quick-start/
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'lioneldb';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
---
+++ students.js
@@ -0,0 +1,62 @@
+//one student
+//db.student.insert(student)
+student = {
+    "firstname":"Fred",
+    "lastName":"Friend",
+    "email":"fredterry876@gmail.com",
+    "gender":"M",
+    "country":"SJO",
+    "isStudentActive":true,
+    "favoriteSubjects": [
+        "math",
+        "programming",
+        "it"
+    ],
+    "totalSpentOnBooks":125.00
+}
+
+
+//may student
+//db.student.insertMany(students)
+students = [{
+    "firstname":"Lionel",
+    "lastName":"Jones",
+    "email":"lioneljones5116@gmail.com",
+    "gender":"M",
+    "country":"USA",
+    "isStudentActive":false,
+    "favoriteSubjects": [
+        "math",
+        "programming",
+        "it"
+    ],
+    "totalSpentOnBooks":250.00
+},
+{
+    "firstname":"Rick",
+    "lastName":"James",
+    "email":"slickRick@gmail.com",
+    "gender":"M",
+    "country":"Phillipenes",
+    "isStudentActive":true,
+    "favoriteSubjects": [
+        "singing",
+        "dancing",
+        "music"
+    ],
+    "totalSpentOnBooks":50.00
+},
+{
+    "firstname":"Mark",
+    "lastName":"Tyson",
+    "email":"pimpDaddy@gmail.com",
+    "gender":"M",
+    "country":"DR",
+    "isStudentActive":true,
+    "favoriteSubjects": [
+        "whoreing",
+        "pimping",
+        "eating"
+    ],
+    "totalSpentOnBooks":125.00
+}]
